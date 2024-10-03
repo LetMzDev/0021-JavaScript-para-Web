a3cd87a620f08ea581ae874a2ce46a79e6af4b4a
Rafa MZ <mz.tears.of.time@gmail.com>
2024-10-03 12:34:22 -0300

    25. Correção ao excluir item da lista

--- js/verificar_lista_vazia.js
+++ js/verificar_lista_vazia.js
@@ -2,7 +2,7 @@ const mensagem_Lista_Vazia = document.getElementById ( "mensagem-lista-vazia" );
 
 export function Verificar_Lista_Vazia ( lista )
 {
-	if ( lista.childElementCount === 0 )
+	if ( lista.querySelectorAll ( "li" ).length === 0 )
 		mensagem_Lista_Vazia.style.display = "block";
 
 	else
